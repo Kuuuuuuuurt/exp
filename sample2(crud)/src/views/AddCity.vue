68680a4510a5ece8c57b9d75be79d1027e33773e
kuuuuuuuurt <kurtvonn98@gmail.com>
2022-02-27 23:45:31 +0800

    crud with id

--- sample2(crud)/src/views/AddCity.vue
+++ sample2(crud)/src/views/AddCity.vue
@@ -4,40 +4,40 @@
 
     <form @submit.prevent="createCity">
       <div><input type="text" placeholder="City Name" v-model="name" /></div>
-      <div><input type="text" placeholder="Province" v-model="province"/></div>
-      <div><input type="text" placeholder="Country" v-model="country"/></div>
+      <div><input type="text" placeholder="Province" v-model="province" /></div>
+      <div><input type="text" placeholder="Country" v-model="country" /></div>
       <div><input type="submit" /></div>
     </form>
   </div>
 </template>
 
 <script>
-import {citiesColRef, } from '../firebase/firebase';
-import {addDoc} from 'firebase/firestore';
+import { citiesColRef, app } from "../firebase/firebase";
+import { addDoc, getFirestore, setDoc, doc } from "firebase/firestore";
 
 export default {
-    data(){
-        return{
-            
-                name: null,
-                province: null,
-                country: null,
-          
-        };
+  data() {
+    return {
+      name: null,
+      province: null,
+      country: null,
+    };
+  },
+
+  methods: {
+    async createCity() {
+      const db = getFirestore(app);
+
+
+      console.log("Creating Data");
+      const addedDoc = await addDoc(citiesColRef, this.$data);
+      const addedDocs = await setDoc(doc(db, "sample", this.$data.name),this.$data)
+      alert("Document Added Successfully");
+      console.log(addedDoc);
+       console.log(addedDocs);
+
+      this.$router.push("/");
     },
-
-    
-
-    methods:{
-        async createCity(){
-            console.log("Creating Data");
-            const addedDoc = await addDoc(citiesColRef, this.$data);
-            alert('Document Added Successfully');
-            console.log(addedDoc);
-            this.$router.push('/');
-        }
-
-        
-    }
+  },
 };
 </script>
